ea05753a8e76e5be8b0e3e686ca770e0ae9b9473
Oleg Antonov <olega@wix.com>
2019-04-12 15:52:22 +0300

    Integrated webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	target: 'node',
+	entry: './src/index.ts',
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.ts']
+	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				uglifyOptions: {
+					output: {
+						comments: false
+					}
+				}
+			})
+		]
+	},
+	output: {
+		globalObject: 'this',
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'dist'),
+		library: 'rollouter',
+		libraryTarget: 'umd'
+	}
+};
